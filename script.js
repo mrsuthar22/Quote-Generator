e802fa2cfe68a2ccb4376d85a86a4f80790ff76d
Rakesh Suthar <118597548+mrsuthar22@users.noreply.github.com>
2024-01-23 22:23:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function complete() {
 async function getQuote() {
     loading();
     const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
